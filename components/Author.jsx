f95704428f8eee687eece9495493969f33ddc88f
= <=>
2022-12-08 14:43:12 +0700

    first commit

---
+++ components/Author.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from 'next/image'
+import { graphCMSImageLoader } from '../utils'
+
+function Author({ author }) {
+    return (
+        <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+            <div className="absolute flex justify-center -top-14 left-0 right-0">
+                <Image
+                    unoptimized
+                    loader={graphCMSImageLoader}
+                    alt={author.name}
+                    height={100}
+                    width={100}
+                    className="align-middle rounded-full"
+                    src={author.photo.url}
+                />
+            </div>
+            <h3 className="text-white mt-4 mb-4 text-xl font-bold">{author.name}</h3>
+            <p className="text-white text-ls">{author.bio}</p>
+        </div>
+    )
+}
+
+export default Author
